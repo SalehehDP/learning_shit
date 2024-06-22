bba84a9737ccb95637029b85e081cc781f4218dc
Saleheh <Saleheh.dp@gmail.com>
2024-06-22 11:01:43 +0330

    first commit

---
+++ javascript/trying_app_import_export_in_JS/assets/scripts/app.js
@@ -0,0 +1,20 @@
+//console.log('hello testing')
+
+
+// 1 : Responsive import syntax
+// import { lullaby } from "./util.js";  // * we do'net need .js in reat apps 
+
+
+// 2 :
+//import shut from "./util.js"; // give default value a name, use when you have just one function ,value , ... 
+
+
+// 3 : group imports
+//import {one, two} from "./util.js";
+// use as
+//import * as numbers from "./utils.js";// numbers.two  , object
+
+
+import { lullaby } from "./util.js"
+
+console.log(lullaby)
---
+++ javascript/trying_app_import_export_in_JS/assets/scripts/util.js
@@ -0,0 +1,12 @@
+//  1 :
+//export let lullaby = "lalalai"; //exporting a value with name 
+
+
+// 2 :
+// export default "shhhhh"; // just one in any file 
+
+// export let one = 1;
+// export let two = 2;
+
+
+export let lullaby = "lalalai"; //exporting a value with name 
