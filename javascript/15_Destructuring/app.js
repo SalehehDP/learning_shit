f8f692579fac15b4392c453d3519c05b26bbce12
Saleheh <Saleheh.dp@gmail.com>
2024-06-25 13:10:53 +0000

    add destructring doc

---
+++ javascript/15_Destructuring/app.js
@@ -0,0 +1,8 @@
+// Array
+const userName = ["Max", "Amini"];
+//const firstName = userName[0];
+//const lastName = userName[1];
+
+const [fisrtName, lastName] = ["Max", "Amini"];
+
+consol.log(fisrtName);
