807283e7fa18cae34a1b9a2a9375212c976b9e34
Saleheh <Saleheh.dp@gmail.com>
2024-06-25 13:22:02 +0000

    add objects doc

--- javascript/15_Destructuring/app.js
+++ javascript/15_Destructuring/app.js
@@ -3,6 +3,30 @@ const userName = ["Max", "Amini"];
 //const firstName = userName[0];
 //const lastName = userName[1];
 
-const [fisrtName, lastName] = ["Max", "Amini"];
+const [fisrtName, lastName] = ["Max", "Amini"]; //easy syntax to use for shortened
+// pull out bu position
 
 consol.log(fisrtName);
+
+//Object
+
+// const user = {
+//   age: 50,
+//   name: "azam",
+// };
+// const name = user.name;
+// const age = user.age;
+
+const { age, name } = {
+  age: 50,
+  name: "azam",
+}; // name should be same as proptosis name in objects
+// pullout by key name ...
+
+//diffrents name
+const { age: yearsLiveInErath, name: firstName } = {
+  age: 50,
+  name: "azam",
+}; // name should be same as proptosis name in objects
+// pullout by key name ...
+consol.log(firstName);
