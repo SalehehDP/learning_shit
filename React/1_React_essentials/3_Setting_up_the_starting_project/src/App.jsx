bc79bc407a6b81745ef2ba93606e1ef8172c979c
Saleheh <Saleheh.dp@gmail.com>
2024-07-02 13:32:39 +0330

    change on app until 14 videos

--- React/1_React_essentials/3_Setting_up_the_starting_project/src/App.jsx
+++ React/1_React_essentials/3_Setting_up_the_starting_project/src/App.jsx
@@ -1,9 +1,20 @@
 import reactImg from "./assets/react-core-concepts.png"; // don't get lost 
+import {CORE_CONCEPTS} from "./data.js"
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRondomInt(max){
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcepts(props){
+  return (  
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 
@@ -27,7 +38,27 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+          <CoreConcepts
+            title = {CORE_CONCEPTS[0].title}
+            description = {CORE_CONCEPTS[0].description}
+            image = {CORE_CONCEPTS[0].image}
+          />
+          <CoreConcepts {...CORE_CONCEPTS[1]}/>
+          <CoreConcepts
+          title = {CORE_CONCEPTS[2].title}
+          description = {CORE_CONCEPTS[2].description}
+          image = {CORE_CONCEPTS[2].image}
+          />
+          <CoreConcepts
+            title = {CORE_CONCEPTS[3].title}
+            description = {CORE_CONCEPTS[3].description}
+            image = {CORE_CONCEPTS[3].image}
+          />
+          </ul>
+        </section>
       </main>
     </div>
   );
--- React/1_React_essentials/3_Setting_up_the_starting_project/src/index.jsx
+++ React/1_React_essentials/3_Setting_up_the_starting_project/src/index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-import "./index.css";
+import "./index.css"; // don't get lost 
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
