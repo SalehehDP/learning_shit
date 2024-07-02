276417bb7bc2a2b9026bb5cb92a3852cc9ae9c0b
Saleheh <Saleheh.dp@gmail.com>
2024-07-02 16:38:48 +0330

    add component with use of children /20

--- React/1_React_essentials/3_Setting_up_the_starting_project/src/App.jsx
+++ React/1_React_essentials/3_Setting_up_the_starting_project/src/App.jsx
@@ -1,6 +1,7 @@
 import {CORE_CONCEPTS} from "./data.js"
 import Header from "./components/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -16,17 +17,19 @@ function App() {
             image = {CORE_CONCEPTS[0].image}
           />
           <CoreConcepts {...CORE_CONCEPTS[1]}/>
-          <CoreConcepts
-          title = {CORE_CONCEPTS[2].title}
-          description = {CORE_CONCEPTS[2].description}
-          image = {CORE_CONCEPTS[2].image}
-          />
-          <CoreConcepts
-            title = {CORE_CONCEPTS[3].title}
-            description = {CORE_CONCEPTS[3].description}
-            image = {CORE_CONCEPTS[3].image}
-          />
+          <CoreConcepts {...CORE_CONCEPTS[2]}/>
+          <CoreConcepts {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id="examples"> 
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+
         </section>
       </main>
     </div>
