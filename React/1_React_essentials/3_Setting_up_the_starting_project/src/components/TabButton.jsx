6892937ded55afaa759105771df38c79eadd9563
Saleheh <Saleheh.dp@gmail.com>
2024-07-18 08:11:16 +0330

    add doc and change in app / 35 dynamic styling

--- React/1_React_essentials/3_Setting_up_the_starting_project/src/components/TabButton.jsx
+++ React/1_React_essentials/3_Setting_up_the_starting_project/src/components/TabButton.jsx
@@ -1,12 +1,12 @@
 
-export default function TabButton({children , onSelect}){ // only children porps name is out off our contorl beside that we can name any props anything we want.
+export default function TabButton({children , onSelect , isSelected}){ // only children porps name is out off our contorl beside that we can name any props anything we want.
     // function handleClick(){
     //     console.log(children)
     // }
 
     return(
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? "active" : ""} onClick={onSelect}>{children}</button>
         </li>
 
     );
