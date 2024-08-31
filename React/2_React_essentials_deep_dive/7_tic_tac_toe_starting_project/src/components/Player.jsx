d92af353013f205ba0e1b89063c9e12f59fad76e
Saleheh <Saleheh.dp@gmail.com>
2024-08-31 10:06:45 +0330

    add doc 18

--- React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/components/Player.jsx
+++ React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/components/Player.jsx
@@ -7,3 +7,5 @@ export default function Player({name , symbol}){
         <button>Edit</button>
     </li>
 }
+
+// any palce that you see somthing is been repeted , or for some change you should do same thing multi times => components
