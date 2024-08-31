4afe930f5e3d7bcabb716499612a212d90e99dc4
Saleheh <Saleheh.dp@gmail.com>
2024-08-31 10:44:57 +0330

    add change and doc 19 (state for edit button)

--- React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/components/Player.jsx
+++ React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 export default function Player({name , symbol}){
+
+    const [ isEditing, setIsEditing ] = useState(false);
+    function handleEditClick(){
+        setIsEditing(true);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    if (isEditing) {
+        playerName = <input type="text" required />;
+    }
     return <li>
-        <span className="palyer">
-            <span className="player-name">{name}</span>
+        <span className="player">
+            {playerName}
             <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleEditClick}>Edit</button>
     </li>
 }
 
 // any palce that you see somthing is been repeted , or for some change you should do same thing multi times => components
+
+//(span should have input field , button name should change ,.....) we need somthing cause the UI to update when changed => State 
+
+// dynamic content => extra variable , if eles
