8735c157b54b405fd83f277ddb750ebc3de42498
Saleheh <Saleheh.dp@gmail.com>
2024-09-03 09:36:07 +0330

    Add Doc and change 27 lifting state up

--- React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/App.jsx
+++ React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/App.jsx
@@ -1,15 +1,22 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer , setActivePlayer] = useState('X');
+
+  function handleSwichingTrun(){
+    setActivePlayer((curPlayer) => curPlayer === 'X' ? 'O' : 'X'); 
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player1" symbol="X"/>
-          <Player initialName="Player2" symbol="O"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSwichingTrun={handleSwichingTrun} activePlayerSymbol={activePlayer} />
       </div>
     </main>
   )
