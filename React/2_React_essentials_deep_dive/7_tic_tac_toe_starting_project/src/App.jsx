ddf509c9bbbe732d6d48c6f4e8015a955a383875
Saleheh <Saleheh.dp@gmail.com>
2024-09-08 11:30:37 +0330

    Add change 32

--- React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/App.jsx
+++ React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/App.jsx
@@ -3,18 +3,24 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { useState } from "react";
 
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer , setActivePlayer] = useState('X');
+  // const [activePlayer , setActivePlayer] = useState('X');
   const [gameTurns , setGameTurns] = useState([]);
 
-  function handleSelectSquare(rowIndex , colIndex){
-    setActivePlayer((curPlayer) => curPlayer === 'X' ? 'O' : 'X'); 
-    setGameTurns((prevTruns) => {
+  const activePlayer = deriveActivePlayer(gameTurns);
 
-      let currentPlayer = "X";
-      if (prevTruns.length > 0 && prevTruns[0].player === 'X'){
-        currentPlayer = "O";
-      }
+  function handleSelectSquare(rowIndex , colIndex){
+    // setActivePlayer((curPlayer) => curPlayer === 'X' ? 'O' : 'X'); 
+    setGameTurns((prevTruns) => {
+      const currentPlayer = deriveActivePlayer(prevTruns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
--- React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/components/Log.jsx
+++ React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/components/Log.jsx
@@ -1,5 +1,3 @@
-
-
 export default function Log({turns}){
 
     return <ol id="log">
