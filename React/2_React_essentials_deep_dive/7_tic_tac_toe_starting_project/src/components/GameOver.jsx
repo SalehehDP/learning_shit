4349f3ad6d1543ee0cd6c81c106eba6917bcd9e0
Saleheh <Saleheh.dp@gmail.com>
2024-09-08 13:00:03 +0330

    fix game over id

--- React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/components/GameOver.jsx
+++ React/2_React_essentials_deep_dive/7_tic_tac_toe_starting_project/src/components/GameOver.jsx
@@ -1,5 +1,5 @@
 export default function GameOver({winner}){
-    return <div id="game-over!">
+    return <div id="game-over">
         <h2>Game over</h2>
         {winner && <p>{winner} won!</p>}
         {!winner && <p>it&apos;s drow!</p>}
